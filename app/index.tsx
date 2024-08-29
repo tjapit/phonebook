cf8445df7d5f703f113e605ead7b365a203fd900
Timothy Japit <timothy.japit@gmail.com>
2024-08-29 19:15:17 +0700

    feat(contacts): get contacts from user's phonebook

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,28 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as Contacts from "expo-contacts";
 
 const App = () => {
+  const [contacts, setContacts] = useState<Contacts.Contact[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        setContacts(data);
+      }
+    })();
+  }, []);
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Hello world!</Text>
+      {contacts.map((contact) => (
+        <Text key={contact.id}>{contact.name}</Text>
+      ))}
     </View>
   );
 };
