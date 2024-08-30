552d1fe6e5014395667071e13c4d33a02d3a73da
Timothy Japit <timothy.japit@gmail.com>
2024-08-30 12:16:12 +0700

    refactor(AppGradient): to accept className

--- components/AppGradient.tsx
+++ components/AppGradient.tsx
@@ -7,14 +7,16 @@ import { StatusBar } from "expo-status-bar";
 interface AppGradientProps {
   children: any;
   colors?: string[];
+  className?: string;
 }
 
 const AppGradient = ({
   children,
   colors = [Colors.grey, Colors.dark],
+  className,
 }: AppGradientProps) => {
   return (
-    <LinearGradient colors={colors} className="flex-1">
+    <LinearGradient colors={colors} className={`flex-1 ${className}`}>
       <SafeAreaView className="flex-1 mx-5 my-3">{children}</SafeAreaView>
       <StatusBar style="light" />
     </LinearGradient>
