8080a9aa985b953a3114599265108bc987e53962
Timothy Japit <timothy.japit@gmail.com>
2024-08-31 17:48:10 +0700

    fix(test): broken <ContactsListScreen /> test
    - added redux and react-redux to `transformIgnorePatterns` in
      package.json
    - configured a `mockStore` with some `initialState` using
      `redux-mock-store`
    - mocked middlewares with `thunk` from `redux-thunk`

--- app/contacts/__tests__/index.test.tsx
+++ app/contacts/__tests__/index.test.tsx
@@ -1,9 +1,29 @@
 import { render } from "@testing-library/react-native";
 import ContactsListScreen from "@/app/contacts/index";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { ContactsState } from "@/store/features/contacts/contactsSlice";
+import { FavoriteState } from "@/store/features/contacts/favoriteSlice";
+import { thunk } from "redux-thunk";
 
-describe("Contacts List", () => {
+describe("<ContactsListScreen />", () => {
   it("should render the contacts list", () => {
-    const { getByText } = render(<ContactsListScreen />);
+    const initialState = {
+      contacts: {
+        data: [],
+        loading: false,
+        error: undefined,
+      } as ContactsState,
+      favorite: { data: undefined } as FavoriteState,
+    };
+    const mockStore = configureStore([thunk as any]);
+    const store = mockStore(initialState);
+    const { getByText } = render(
+      <Provider store={store}>
+        <ContactsListScreen />
+      </Provider>,
+    );
+
     getByText("Contacts");
   });
 });
