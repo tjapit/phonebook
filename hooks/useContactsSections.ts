246ffb01ea12d348cae876bb9aacc1b41805009a
Timothy Japit <timothy.japit@gmail.com>
2024-09-04 23:39:06 +0700

    feat(useContactsSections): useMemo, maps, dep array

--- hooks/useContactsSections.ts
+++ hooks/useContactsSections.ts
@@ -1,34 +1,41 @@
 import { ContactsSection } from "@/constants/models";
 import { useAppDispatch, useAppSelector } from ".";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { fetchContacts } from "@/store/features/contacts/contactsSlice";
 
 export function useContactsSections() {
   const dispatch = useAppDispatch();
-  const { data, loading, error } = useAppSelector((state) => state.contacts);
+  const {
+    data = [], // handle empty data gracefully
+    loading,
+    error,
+  } = useAppSelector((state) => state.contacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
-  }, []);
+  }, [dispatch]); // added dispatch to dep array to avoid potential issues with stale closures
 
-  const contactsSections = data.reduce<ContactsSection[]>(
-    (sections, contact) => {
-      const firstLetter = contact.name[0].toUpperCase();
-      let section = sections.find((sec) => sec.title === firstLetter);
+  // useMemo to avoid recalculating on every render unless data changes
+  const contactsSections = useMemo(() => {
+    const sectionsMap = data.reduce<Record<string, ContactsSection>>(
+      (map, contact) => {
+        const firstLetter = contact.name[0].toUpperCase();
 
-      if (!section) {
-        section = { title: firstLetter, data: [] };
-        sections.push(section);
-      }
+        if (!map[firstLetter]) {
+          map[firstLetter] = { title: firstLetter, data: [] };
+        }
 
-      section.data.push(contact);
+        map[firstLetter].data.push(contact);
 
-      return sections;
-    },
-    [],
-  );
+        return map;
+      },
+      {}, // use map instead of finding the section and pushing data into it
+    );
 
-  contactsSections.sort((a, b) => a.title.localeCompare(b.title));
+    return Object.values(sectionsMap).sort((a, b) =>
+      a.title.localeCompare(b.title),
+    );
+  }, [data]);
 
   return { contactsSections, loading, error };
 }
