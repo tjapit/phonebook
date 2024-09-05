1ec7434994b61dff634d83584acbbad4b7ecdf4c
Timothy Japit <timothy.japit@gmail.com>
2024-09-05 16:26:06 +0700

    fix(delete-contact): toggle favorite if same as deleted contact

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -2,8 +2,9 @@ import { Pressable } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { Contact } from "expo-contacts";
-import { useAppDispatch } from "@/hooks";
+import { useAppDispatch, useAppSelector } from "@/hooks";
 import { deleteContact } from "@/store/features/contacts/contactsSlice";
+import { toggleFavorite } from "@/store/features/contacts/favoriteSlice";
 
 interface DeleteButtonProps {
   className?: string;
@@ -17,11 +18,17 @@ const DeleteButton = ({
   size = 24,
 }: DeleteButtonProps) => {
   const dispatch = useAppDispatch();
+  const { data: favorite } = useAppSelector((state) => state.favorite);
 
   return (
     <Pressable
       className={`active:opacity-80 ${className}`}
-      onPress={() => dispatch(deleteContact(String(selectedContact.id)))}
+      onPress={() => {
+        dispatch(deleteContact(String(selectedContact.id)));
+        if (selectedContact.id === favorite?.id) {
+          dispatch(toggleFavorite(selectedContact));
+        }
+      }}
     >
       <AntDesign name="delete" size={size} color="red" />
     </Pressable>
