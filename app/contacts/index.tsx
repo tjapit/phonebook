3b8ad7ae12edf094ec3c5b96b798fc2f28105d9d
Timothy Japit <timothy.japit@gmail.com>
2024-09-07 14:34:52 +0700

    feat(search-contacts): filter by section

--- app/contacts/index.tsx
+++ app/contacts/index.tsx
@@ -1,11 +1,29 @@
-import { Text, ActivityIndicator } from "react-native";
-import React from "react";
+import { Text, ActivityIndicator, TextInput } from "react-native";
+import React, { useState } from "react";
 import AppGradient from "@/components/AppGradient";
 import { useContactsSections } from "@/hooks/useContactsSections";
 import ContactsList from "@/components/ContactsList";
+import { ContactsSection } from "@/constants/models";
 
 const ContactsListScreen = () => {
+  const [query, setQuery] = useState("");
   const { contactsSections, loading, error } = useContactsSections();
+  const [filteredData, setFilteredData] =
+    useState<ContactsSection[]>(contactsSections);
+
+  const handleChangeText = (text: string) => {
+    setQuery(text);
+
+    if (text === "") {
+      setFilteredData(contactsSections);
+    } else {
+      setFilteredData(
+        contactsSections.filter(
+          (section) => section.title.toLowerCase() === text,
+        ),
+      );
+    }
+  };
 
   if (loading) {
     return (
@@ -24,7 +42,13 @@ const ContactsListScreen = () => {
   return (
     <AppGradient>
       <Text className="my-10 text-4xl text-white font-bold">Contacts</Text>
-      <ContactsList data={contactsSections} />
+      <TextInput
+        className="mb-4 p-4 bg-white rounded-3xl text-xl"
+        value={query}
+        autoCapitalize="none"
+        onChangeText={handleChangeText}
+      />
+      <ContactsList data={filteredData} />
     </AppGradient>
   );
 };
