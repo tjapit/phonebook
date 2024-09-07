b2625c93793d9e6a85b631b0f9507adfd3ffa342
Timothy Japit <timothy.japit@gmail.com>
2024-09-07 22:49:47 +0700

    refactor(favorite): moved favorite to FavoriteBar
    to prevent rerender of the whole list on `toggleFavorite`

--- components/ContactsList.tsx
+++ components/ContactsList.tsx
@@ -7,18 +7,17 @@ import {
 } from "react-native";
 import React from "react";
 import { router } from "expo-router";
-import { useAppDispatch, useAppSelector } from "@/hooks";
+import { useAppDispatch } from "@/hooks";
 import { selectContact } from "@/store/features/contacts/selectedContactSlice";
 import Separator from "./Separator";
-import { AntDesign } from "@expo/vector-icons";
 import FavoriteButton from "./FavoriteButton";
 import DeleteButton from "./DeleteButton";
 import { useContactsSections } from "@/hooks/useContactsSections";
 import SearchBar from "./SearchBar";
+import FavoriteBar from "./FavoriteBar";
 
 const ContactsList = () => {
   const dispatch = useAppDispatch();
-  const { data: favorite } = useAppSelector((state) => state.favorite);
   const { contactsSections, loading, error, query, handleChangeQuery } =
     useContactsSections();
 
@@ -31,34 +30,7 @@ const ContactsList = () => {
   return (
     <View className="flex-1 justify-between gap-2">
       <SearchBar query={query} onQueryChange={handleChangeQuery} />
-      <View className="px-4 bg-black/40 rounded-3xl">
-        <Pressable
-          className="my-4 active:opacity-80"
-          onPress={() => {
-            if (!favorite) {
-              return;
-            }
-            dispatch(selectContact(favorite));
-            router.push(`/contacts/${favorite.id}`);
-          }}
-        >
-          <View className="flex-row items-center gap-x-2">
-            {favorite ? (
-              <>
-                <AntDesign name="star" size={32} color="white" />
-                <Text className="text-xl text-white">{favorite.name}</Text>
-              </>
-            ) : (
-              <>
-                <AntDesign name="staro" size={32} color="white" />
-                <Text className="text-xl text-white/60">
-                  No favorite contact
-                </Text>
-              </>
-            )}
-          </View>
-        </Pressable>
-      </View>
+      <FavoriteBar />
       <SectionList
         sections={contactsSections}
         keyExtractor={(contact, index) => contact.name + index}
---
+++ components/FavoriteBar.tsx
@@ -0,0 +1,37 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { AntDesign } from "@expo/vector-icons";
+import { selectContact } from "@/store/features/contacts/selectedContactSlice";
+import { router } from "expo-router";
+
+const FavoriteBar = () => {
+  const dispatch = useAppDispatch();
+  const { data: favorite } = useAppSelector((state) => state.favorite);
+
+  return (
+    <View className="px-4 bg-black/40 rounded-3xl">
+      {favorite ? (
+        <Pressable
+          className="my-4 active:opacity-80"
+          onPress={() => {
+            dispatch(selectContact(favorite));
+            router.push(`/contacts/${favorite.id}`);
+          }}
+        >
+          <View className="flex-row items-center gap-x-2">
+            <AntDesign name="star" size={32} color="white" />
+            <Text className="text-xl text-white">{favorite.name}</Text>
+          </View>
+        </Pressable>
+      ) : (
+        <View className="my-4 flex-row items-center gap-x-2">
+          <AntDesign name="staro" size={32} color="grey" />
+          <Text className="text-xl text-white/60">No favorite contact</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default FavoriteBar;
